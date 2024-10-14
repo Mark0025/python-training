d119225630ce07e003f6b574a8b31874932f66d4
Mark Carpenter <Markcarpenter0025@gmail.com>
2024-10-14 18:09:13 -0500

    Initial commit

---
+++ excercise-1.py
@@ -0,0 +1,16 @@
+print("Welcome to Python programming!")
+
+age = 38
+print("My age is " + str(age))
+
+x = 4.0
+y = 7.0
+print("The sum of x + y is", x + y)
+
+triangle_area = (x * y) / 2
+print("The area of the triangle is", triangle_area)
+
+
+
+
+
